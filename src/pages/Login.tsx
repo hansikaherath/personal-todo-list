237be2d08b9c376861005a3457f24d43e7aabde3
edpchinthana <edpchinthana@outlook.com>
2020-10-13 00:52:42 +0530

    login ui state

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,26 +1,65 @@
-import React,{Component} from 'react';
+import React, {Component, FormEvent} from 'react';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 
-export default class Login extends Component{
+interface IProps {
+}
+
+interface IState {
+    email?: String,
+    password?: String,
+    stayLoggedIn?: Boolean
+}
+
+export default class Login extends Component<IProps, IState>{
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            email:'',
+            password:'',
+            stayLoggedIn:false
+        }
+    }
+
+    onEmailChange = (value: String) => {
+        this.setState({email: value});
+    }
+
+    onPasswordChange = (value: String) => {
+        this.setState({password: value});
+    }
+
+    onStayLoggedInChange = () => {
+        this.setState({stayLoggedIn: !this.state.stayLoggedIn});
+    }
+
+    onSubmit = (event: any) => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
     render(){
         return (
             <Container className={"bg-light p-4 "}>
                 <div className={"col-lg-6"}>
                     <h2>User Login</h2><br/><br/>
-                    <Form>
+                    <Form onSubmit={(e) => this.onSubmit(e)}>
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control type="email" placeholder="Enter email"
+                               onChange={(e) => this.onEmailChange(e.target.value)}/>
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control type="password" placeholder="Password"
+                                onChange={(e) => this.onPasswordChange(e.target.value)}
+                            />
                         </Form.Group>
                         <Form.Group controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Stay logged in" />
+                            <Form.Check type="checkbox" label="Stay logged in"
+                            onChange={()=> this.onStayLoggedInChange()}/>
                         </Form.Group>
                         <Button variant="primary" type="submit">
                             Login
